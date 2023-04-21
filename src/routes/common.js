da9da8d784224428dc0dbf18f8f03b16acb264c8
SK-ROHIT-HOSSAIN <skrohithossain2000@gmail.com>
2023-04-21 15:26:45 +0530

    src uploaded

--- src/routes/common.js
+++ src/routes/common.js
@@ -1,7 +1,8 @@
 const name = "FunctionUp"
 
-const doSomething = function () {
+const doSomething = function() {
     console.log('I did something')
 }
 
 module.exports.name = name
+module.exports.doSomething = doSomething
