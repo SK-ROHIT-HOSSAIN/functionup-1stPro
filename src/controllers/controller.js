14c8faacc5ce88568703fbaa88a4aa50b47288cb
SK-ROHIT-HOSSAIN <126488100+SK-ROHIT-HOSSAIN@users.noreply.github.com>
2023-05-09 17:59:16 +0530

    Updated controller.js

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -4,10 +4,10 @@ const ordermodel = require('../models/ordermodel')
 
 const usercreate = async(req, res) => {
     //convert string into boolean using boolean function
-    let isFreeAppUser = Boolean(req.headers.isfreeappuser);
+    // let isFreeAppUser = Boolean(req.headers.isfreeappuser);
     let data = req.body;
     //set the field value with headers key value
-    data.isFreeAppUser = isFreeAppUser
+    data.isFreeAppUser = req.headers.isfreeappuser;
     let saveddata = await usermodel.create(data);
     res.send(saveddata)
 
@@ -18,33 +18,55 @@ const productcreate = async(req, res) => {
     res.send(saveddata)
 }
 const ordercreate = async(req, res) => {
-    const { userId, productId, amount, isFreeAppUser, date } = req.body;
-    let userid_exist = await usermodel.findById({ _id: userId });
-    let product_exist = await productmodel.findById({ _id: productId });
+    const data = req.body;
+    data.isFreeAppUser = req.headers.isfreeappuser;
+    let userid_exist = await usermodel.findById({ _id: req.body.userId });
+    let product_exist = await productmodel.findById({ _id: req.body.productId });
     if (!userid_exist) {
         return res.send({ message: "user id not exist" })
     }
     if (!product_exist) {
         return res.send({ message: "product is not exist" })
     }
-    let saveddata = await ordermodel.create({ userId, productId, amount, isFreeAppUser, date })
+    let saveddata = await ordermodel.create(data)
     return res.send(saveddata)
 }
 const purchase = async(req, res) => {
-    let isFreeAppUser = Boolean(req.headers.isfreeappuser);
-    let subs_price = 200;
+    // let isFreeAppUser = Boolean(req.headers.isfreeappuser);
+    let orderDetails = await ordermodel.findById({ _id: "645a2f75c999e361bfd5e4c6" })
+    let orderAmount = orderDetails.amount;
+    let userId = orderDetails.userId;
+    let pId = orderDetails.productId;
+    let isFreeAppUser = orderDetails.isFreeAppUser;
 
-    if (isFreeAppUser === true) {
-        let amount = await ordermodel.findOneAndUpdate({ isFreeAppUser: true }, { $set: { amount: 0 } });
+    let purchaseDetails = await productmodel.findById({ _id: pId })
+    let pAmount = purchaseDetails.price;
+
+    let userDetails = await usermodel.findById({ _id: userId })
+    let userBalance = userDetails.balance;
+
+    if (isFreeAppUser === true)
+
+    {
+
+        let amount = await ordermodel.findOneAndUpdate({ isFreeAppUser: true }, { $set: { amount: 0 } }, { new: true });
         return res.send(amount);
-    }
-    if (isFreeAppUser === false) {
-        let updated = await ordermodel.findOneAndUpdate({ isFreeAppUser: false }, { $set: { amount: amount - subs_price } })
-        return res.send(updated);
-    } else {
-        return res.send("insufficient balance");
-    }
 
+    } else {
+        let updated = await ordermodel.findOneAndUpdate({ isFreeAppUser: false }, { $set: { amount: pAmount } }, { new: true })
+            //let updateMore = await ordermodel.findOneAndUpdate({ isFreeAppUser: false }, { $set: {isFreeAppUser: true  } }, { new: true })
+            // if (!updated) {
+            //     return res.send(updated);
+            // }
+
+        if (orderAmount <= userBalance) {
+            let userUpdate = await usermodel.findOneAndUpdate({ _id: userId }, { $set: { balance: userBalance - orderAmount } }, { new: true })
+            res.send("updated")
+        } else {
+            return res.send("insufficient balance");
+        }
+
+    }
 }
 module.exports.usercreate = usercreate
 module.exports.productcreate = productcreate
